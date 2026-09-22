b2cc375c2b2be723b35fe353e5d597943c5736dd
Taigo Ito <qwel.design@gmail.com>
2026-09-22 11:37:12 +0900

    feat: 編成画面UIの初期構成

---
+++ src/parts/Setup/index.tsx
@@ -0,0 +1,28 @@
+// src/parts/Setup/index.tsx
+
+import { useParams, Link } from 'react-router-dom'
+import List from '../List'
+import Detail from '../List/Detail'
+import { SAMPLE_CHARACTERS as samples } from '../../domains/Character'
+
+function Setup() {
+  // uid があれば1人のサンプルを探す
+  const { uid } = useParams()
+  const sample = samples.find(m => m.id === Number(uid))
+
+  return (
+    <div className="px-6">
+      {!sample
+        ? <List units={samples} />
+        : (
+          <>
+            <Detail unit={sample} />
+            <Link className="ms-12 italic" to="/setup/">&lt; Back to list</Link>
+          </>
+        )
+      }
+    </div>
+  )
+}
+
+export default Setup
