ce3310c541ebef22cdd014d68cea40de59f32869
Taigo Ito <qwel.design@gmail.com>
2026-09-23 13:27:28 +0900

    feat: 戦闘ユニットの管理を司るクラスを準備

---
+++ src/domains/Combat/Unit/index.ts
@@ -0,0 +1,46 @@
+// src/domains/Combat/Unit/index.ts
+
+import { Equipments } from '../../Character'
+
+const combatIds: number[] = [1, 2, 3, 4, 5, 6, 7, 8] as const
+
+export const SIDE_KEYS = ['player', 'enemy'] as const
+
+export const POSITION_KEYS = ['back', 'left', 'center', 'right'] as const
+
+// 戦闘ユニットID
+export type CombatId = typeof combatIds[number]
+
+// 戦闘ユニットの所属
+export type Side = typeof SIDE_KEYS[number]
+
+// 戦闘ユニットの配置
+export type Position = typeof POSITION_KEYS[number]
+
+// 戦闘ユニットモデル
+export type CombatUnitModel = {
+  name: string
+  maxHp: number
+  level: number
+  dmgMod: number
+  ev: number
+  pre: number
+  mre: number
+  equipments: Equipments
+}
+
+// 戦闘ユニットの管理を司るクラス
+export class CombatUnit {
+  public combatId: CombatId
+  public name: string
+  public side: Side
+  public position: Position
+
+  constructor(model: CombatUnitModel, combatId: CombatId) {
+    const { name } = model
+    this.combatId = combatId
+    this.name = name
+    this.side = combatId <= 4 ? 'player' : 'enemy'
+    this.position = 'back'
+  }
+}
